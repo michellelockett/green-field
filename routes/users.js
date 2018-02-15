4a7fcb5b70f559a8adb98de7eb45ff3b8bd67c5a
Chris Poole <poolecs@gmail.com>
2018-02-15 14:07:49 -0800

    Update User routes

--- routes/users.js
+++ routes/users.js
@@ -3,24 +3,28 @@
 // create a user
 app.post('/users', (req, res) => {
   // direct to method in Users controller/model handler
+  res.send('This will return an object containing a status message confirming creation of the specific user.');
 });
 
 // read all users
 // note: may be deprecated
 app.get('/users', (req, res) => {
   // direct to method in Users controller/model handler
+  res.send('This will return an object containing a status message and a list of all users.');
 });
 
 // read a specific user
 app.get('/users/:id', (req, res) => {
   // direct to method in Users controller/model handler
   // passing :id from params
+  res.send('This will return an object containing a status message and selected information for a specific user.');
 });
 
 // update a specific user
 app.put('/users/:id', (req, res) => {
   // direct to method in Users controller/model handler
   // passing :id from params
+  res.send('This will return an object containing a status message and updated information for a specific user.');
 });
 
 // destroy a user account
@@ -28,9 +32,19 @@ app.put('/users/:id', (req, res) => {
 app.delete('/users/:id', (req, res) => {
   // direct to method in Users controller/model handler
   // passing :id from params
+  res.send('This will return an object containing a status message confirming deletion of the specific user.');
+});
+
+// signin
+app.post('/signin', (req, res) => {
+  // direct to method in Users (or Authentication) controller/modle handler
+  res.send('This will return an object containing a status message confirming login.');
+  // Note: may choose to issue a server-originating redirect.
 });
 
 // logout
 app.post('/logout', (req, res) => {
   // direct to method in Users (or Authentication) controller/modle handler
+  res.send('This will return an object containing a status message confirming logout.');
+  // Note: may choose to issue a server-originating redirect.
 });
