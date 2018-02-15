c771149ff2b8c5991f78117dd18a0057d73eb394
Chris Poole <poolecs@gmail.com>
2018-02-15 13:20:47 -0800

    Sketch out simple User routes

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,36 @@
 //routes for users of the book app interacting with the server
+
+// create a user
+app.post('/users', (req, res) => {
+  // direct to method in Users controller/model handler
+});
+
+// read all users
+// note: may be deprecated
+app.get('/users', (req, res) => {
+  // direct to method in Users controller/model handler
+});
+
+// read a specific user
+app.get('/users/:id', (req, res) => {
+  // direct to method in Users controller/model handler
+  // passing :id from params
+});
+
+// update a specific user
+app.put('/users/:id', (req, res) => {
+  // direct to method in Users controller/model handler
+  // passing :id from params
+});
+
+// destroy a user account
+// note: may be deprecated
+app.delete('/users/:id', (req, res) => {
+  // direct to method in Users controller/model handler
+  // passing :id from params
+});
+
+// logout
+app.post('/logout', (req, res) => {
+  // direct to method in Users (or Authentication) controller/modle handler
+});
