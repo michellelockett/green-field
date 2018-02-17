dece8880e8101868e2ee09386988b6ec8dcb4bfd
michellelockett <shelldog42@gmail.com>
2018-02-16 20:34:28 -0800

    adds components for booklist and bookcover list.

--- client/components/app.js
+++ client/components/app.js
@@ -1,3 +1,9 @@
 angular.module('BookApp').component('app', {
-  templateUrl: "./templates/app.html"
+  templateUrl: "./templates/app.html",
+  controller: function() {
+    this.$onInit = () => {
+      this.books = window.books;
+      this.view = 'list';
+    }
+  }
 });
