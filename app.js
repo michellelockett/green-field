7f6121c9c0db036a42eca588da7b802281635186
Chris Poole <poolecs@gmail.com>
2018-02-19 14:04:24 -0800

    Implement simple Express server.

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//node express server
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const {userRoutes} = require('./routes/users');
+const {apiRoutes} = require('./routes/api');
+
+const app = express();
+
+app.use('*', (req, res, next) => {
+  console.log(`${req.method} request received for ${req.url}`);
+  next();
+});
+
+app.use('/users', userRoutes);
+app.use('/api', apiRoutes);
+
+app.listen(3000, () => {
+  console.log(`Server listening on port 3000`);
+});
