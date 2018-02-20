bd5e898197b17277d7e9fa4e73fc1d82c334e600
Chris Poole <poolecs@gmail.com>
2018-02-19 17:57:06 -0800

    Implement API calls to Classify, Google

--- controllers/api.js
+++ controllers/api.js
@@ -4,7 +4,11 @@ const xml2js = require('xml2js');
 const {API_KEY} = require('../config/google');
 
 const apiController = {
-  getBookData(req, res) {
+  getBookData(isbn, res) {
+
+    // declare book object to hold the results of successive API calls
+    let book = {};
+
     axios.get(`http://classify.oclc.org/classify2/Classify?oclc=57358293&summary=true`)
     .then((response) => {
       // convert xml to json
@@ -12,26 +16,68 @@ const apiController = {
         if (err) {
           console.log("Error: ", err);
         }
+        // assign precise Dewey Decimal # to book object
 
-        // let ddc = result.classify.recommendations[0].ddc[0].mostPopular[1]['$'].nsfa;
-        console.log(result.classify.work.author);
-        console.log(result.classify.work.author);
-        console.log(result.classify.work.author);
+        /**
+        Classify API is inconsistent in number of classifications
+        or order. The precise location of the DDC may change from one API call to
+        next.
 
-// https://www.googleapis.com/books/v1/volumes?q=search+terms
+        The below function:
+        1. iterates through each of the 'mostPopular' classification objects
+        2. it accesses the 'nsfa' and 'sfa' properties of each
+        3. once it locates a property that is a string number containing a decimal,
+        it stops and records that as the dewey classification.
+        4. if no dewey classification is located, 'unavailable' is assigned to DDC.
+        **/
 
+        const extractDDC = () => {
+          let classifications = result.classify.recommendations[0].ddc[0].mostPopular;
+          classifications.forEach((item, index) => {
+            if (item['$'].nsfa && item['$'].nsfa.includes('.')) {
+              console.log('DDC FOUND!', item['$'].nsfa);
+              return item['$'].nsfa;
+            } else if (item['$'].sfa && item['$'].sfa.includes('.')) {
+              console.log('DDC FOUND!', item['$'].sfa);
+              return item['$'].sfa;
+            } else {
+              return 'unavailable';
+            }
 
+          });
+        };
+
+        // result.classify.recommendations[0].ddc[0].mostPopular[1]['$'].nsfa
+
+        book.ddc = extractDDC();
+        // console.log(result.classify.recommendations[0].ddc[0].mostPopular);
       });
     })
     .then((jsonData) => {
-      return axios.get(`https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=${API_KEY}`);
+      // make a subsequent API call to Google Books API
+      return axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn=${isbn}&key=${API_KEY}`);
     })
     .then((response) => {
-      // do something with JSON data
-      console.log(response.data);
+      // variable to hold data from first returned book
+      // that matches ISBN
+      let bookData = response.data.items[0].volumeInfo;
+
+      // attach properties to book
+      book.isbn = isbn;
+      book.title = bookData.title;
+      book.authors = bookData.authors;
+      book.published = bookData.publishedDate;
+      book.description = bookData.description;
+      book.pages = bookData.pageCount;
+      book.format = bookData.printType;
+      book.genre = bookData.categories;
+      book.cover = bookData.imageLinks.thumbnail;
+
+      // send book object back to client
+      res.send(book);
     })
     .catch((err) => {
-      console.log('getBookData ERROR: ', err);
+      // console.log('getBookData ERROR: ', err);
     })
 
   }
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,13 @@
 /** ROUTES FOR ACTIONS TRIGGERING EXTERNAL API CALLS **/
 const express = require('express')
 const router = express.Router()
-// const { apiController } = require('../controllers/api');
+const { apiController } = require('../controllers/api');
 
 // Receive ISBN from client
 // Create book if doesn't exist
 // Add book association to user
-router.get('/isbn/:id', (req, res) => {
-  // apiController.getBookData(req.params.id);
+router.get('/isbn/:isbn', (req, res) => {
+  apiController.getBookData(req.params.isbn, res);
 });
 
 exports.apiRoutes = router;
