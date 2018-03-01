3d8c127b3a307bb28af8f9bfca91e275e02aa76f
jcerda <jccer103@gmail.com>
2018-03-01 17:04:51 -0600

    updated controllers adding createNewUsers, getUsers, getUserId, and UpdateUsers

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,15 @@ const {User, Book, BookUsers} = require('../models/index');
 const buildBookList = require('../helpers/files');
 
 const userController = {
+  createNewUser(req, res) {
+    User
+      .findOrCreate({where: {firstName: req.body.firstName, lastName: req.body.lastName, userName: req.body.userName, password: req.body.password}})
+      .spread((user, create) => {
+        console.log(user.get({
+          plain: true
+        }))
+      })
+  },
   getUsers(req, res) {
     User.findAll()
       .then((users) => {
@@ -11,8 +20,35 @@ const userController = {
         console.log(err);
       });
   },
+  getUsersId(req, res) {
+    User.findAll({where: { id : req.params.id}})
+      .then((user) => {
+        res.send(user);
+      }) 
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  updateUserId(req, res) {
+  // what exactly was supposed to be updated here
+  // currently will update any data passed in put request for specific user id
+    User.update({firstName: req.body.firstName, lastName: req.body.lastName, userName: req.body.userName, password: req.body.password}, { where: {id: req.params.id}})
+      .then(user => {
+        console.log(user)
+    })
+  }, 
+  deleteUser(req, res) {
+    User.destroy({where: { id: req.params.id}})
+      .then(user => {
+        res.send(user)
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  },
   postBookUser(req,res) {
-
+  // trying to use add methods that should be available with the belongsToMany associations
+  //User.addBook(Book, {where: {id: req.params.id} } )
   },
   getUserWithBooks(req, res) {
     let userId = req.params.id;
