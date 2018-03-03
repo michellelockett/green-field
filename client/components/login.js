bb14de25a5d79960a7f6fdec6ef1f700248b2c27
michellelockett <shelldog42@gmail.com>
2018-03-03 15:51:00 -0800

    Adds signup fucntionality on login view

--- client/components/login.js
+++ client/components/login.js
@@ -1,16 +1,29 @@
 angular.module('BookApp').component('login', {
   templateUrl: './templates/login.html',
   bindings: {
-    submit: '<'
+    submit: '<',
+    signup: '<'
   },
   controller: function($scope) {
     $scope.username = '';
     $scope.password = '';
+    $scope.newFirstName = '';
+    $scope.newLastName = '';
+    $scope.newUsername = '';
+    $scope.newPassword = '';
 
     this.handleSubmit = () => {      
       this.submit($scope.username, $scope.password);  
       $scope.username = '';
       $scope.password = ''; 
+    };
+
+    this.handleNewUser = () => {
+      this.signup($scope.newFirstName, $scope.newLastName, $scope.newUsername, $scope.newPassword);
+      $scope.newFirstName = '';
+      $scope.newLastName = '';
+      $scope.newUsername = '';
+      $scope.newPassword = '';
     };
   }
 });
